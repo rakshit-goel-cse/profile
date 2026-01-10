1138841764fa3d83c73dc17db943cdbe3f825ff9
Rakshit <goelrakshit.er@gmail.com>
2026-01-11 05:02:24 +0530

    updated import

--- profile/src/app.jsx
+++ profile/src/app.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'preact/hooks'
 import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from './vite.svg'
 import './app.css'
 
 export function App() {
