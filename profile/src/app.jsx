54274e868cd868933baff349220b034e70b49c71
Rakshit <goelrakshit.er@gmail.com>
2026-01-11 14:39:47 +0530

    updated import

--- profile/src/app.jsx
+++ profile/src/app.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'preact/hooks'
 import './app.css'
-import MenuBar from './components/menubar'
+import MenuBar from './components/MenuBar'
 import MainPage from './components/MainPage'
 
 export function App() {
